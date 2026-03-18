d3ae523bbed81a4afde6379163749f3ddf4d49bb
Theo Gabriel <theonbgabriel@gmail.com>
2026-03-18 14:57:59 -0300

    first commit

---
+++ src/main/java/com/grupoum/projeto_fera/ProjetoFeraApplication.java
@@ -0,0 +1,13 @@
+package com.grupoum.projeto_fera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFeraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupoum/projeto_fera/ProjetoFeraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoum.projeto_fera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
